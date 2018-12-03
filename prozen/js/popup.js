a08b49b5a7adb098ccd9fe7735858f8a3ba24586
fromgate <fromgate@gmail.com>
2018-12-03 22:13:59 +0300

    Popup displays extension version now

---
+++ prozen/js/popup.js
@@ -0,0 +1,8 @@
+window.browser = (function () {
+    return window.msBrowser ||
+        window.browser ||
+        window.chrome;
+})();
+const nameVersion = document.getElementById("extver");
+nameVersion.innerText = nameVersion.innerText.replace("1.0.0", chrome.runtime.getManifest().version);
+
