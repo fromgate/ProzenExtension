082dd281848b6e5c85591a22d3b06331cf21220a
fromgate <fromgate@gmail.com>
2019-02-05 23:12:01 +0300

    Search in post. Show publication type as icon in search results

--- prozen/js/search.js
+++ prozen/js/search.js
@@ -1,4 +1,5 @@
-const API_URL = "https://zen.yandex.ru/api/v3/launcher/more?country_code=ru&";
+const API_URL = "https://zen.yandex.ru/api/v3/launcher/more?country_code=ru&clid=700&";
+
 const VISIABLE = ["search_msg","spinner","search_result","search_msg_empty","search_not_found"];
 const SEARCH_PLACEHOLDER = ["кора осины", "продзен", "варенье из огурцов", "смысл жизни", "показы"];
 
@@ -62,7 +63,7 @@ function executSearch() {
         showElement("search_result");
         for (let i = 0; i < foundCards.length; i++) {
             const card = foundCards[i];
-            addSearchResult(card.title, card.description, card.url);
+            addSearchResult(card);
         }
     } else {
         showElement("search_not_found");
@@ -78,20 +79,25 @@ function loadPublicationsAndSearch () {
 }
 
 async function loadPageData(initUrl) {
+    const header = new Headers({
+        "Zen-Client-Experiments": "zen-version:2.32.0",
+        "Zen-Features": "{\"no_amp_links\":true,\"forced_bulk_stats\":true,\"blurred_preview\":true,\"big_card_images\":true,\"complaints_with_reasons\":true,\"pass_experiments\":true,\"video_providers\":[\"yandex-web\",\"youtube\",\"youtube-web\"],\"screen\":{\"dpi\":241},\"need_background_image\":true,\"color_theme\":\"white-background\",\"no_small_auth\":true,\"need_main_color\":true,\"need_zen_one_data\":true,\"interests_supported\":true,\"return_sources\":true,\"screens\":[\"feed\",\"category\",\"categories\",\"profile\",\"switchable_subs\",\"suggest\",\"blocked\",\"preferences\",\"blocked_suggest\",\"video_recommend\",\"language\",\"comments_counter\"],\"stat_params_with_context\":true,\"native_onboarding\":true,\"card_types\":[\"post\"]}"
+    });
+
     let url = initUrl;
     const cards = [];
     while (true) {
-        const request = await fetch(url);
+        const request = await fetch(url, {headers : header, method : "GET"});
         let json;
         try {
             json = await request.json();
         } catch (e) {
         }
-        if (!request.ok || json === undefined) {
+        if (!request.ok || json === undefined || json.items === undefined) {
             break;
         }
         const items = json.items;
-        for (let i = 0; i< items.length; i++) {
+        for (let i = 0; i < items.length; i++) {
             const item = items[i];
             if (item.link.startsWith("https://zen.yandex.ru")) {
                 cards.push(cardFromItem(item));
@@ -108,30 +114,76 @@ function randomizeSearchPlaceholer() {
 
 function cardFromItem(item) {
     const card = {};
+    card.type = item.type; // card — статья, story — нарратив, post — post, gif — видео
     card.url = item.link.split("?")[0];
-    card.title = item.title;
-    card.description = item.text;
+    card.title  = card.type === "post" ? postJsonToText(item.rich_text.json) : item.title;
+    card.description = card.type === "post" ? "" : item.text;
     publications.push(card);
     return card;
 }
 
-function addSearchResult(title, description, url) {
+function postJsonToText(json) {
+    if (json === undefined || json.length === 0) {
+        return "";
+    }
+    let str;
+    for (let i = 0; i < json.length; i++) {
+        const obj = json[i];
+        if (obj.type === "text") {
+            if (str === undefined) {
+                str = obj.data
+            } else {
+                str = str + " " +obj.data;
+            }
+        }
+    }
+    return str;
+}
+
+function addSearchResult(card) {
     const a = document.createElement("a");
-    a.setAttribute("href",url);
+    a.setAttribute("href",card.url);
     a.setAttribute("target","_blank");
-    const div = document.createElement("div");
-    div.setAttribute("class", "section");
-    const strong = document.createElement("strong");
-    strong.innerText = title;
-    div.appendChild(strong);
-    div.appendChild(document.createElement("br"));
-    const span = document.createElement("span");
-    span.innerText = description === undefined || description.length === 0 ? "Описание не указано" : description;
-    div.appendChild(span);
+    const div = cardToDiv(card);
     a.appendChild(div);
     const searchResult = document.getElementById("search_result");
     searchResult.appendChild(a);
     searchResult.appendChild(document.createElement("hr"));
+}
+
+function cardToDiv(card) {
+    const div = document.createElement("div");
+    div.setAttribute("class", "section");
+    const icon = document.createElement("span");
+    switch (card.type) {
+        case "card":
+            icon.setAttribute("class","icon_views span_icon");
+            icon.setAttribute("title", "Статья");
+            break;
+        case "story":
+            icon.setAttribute("class","icon_narrative span_icon");
+            icon.setAttribute("title", "Нарратив");
+            break;
+        case "gif":
+            icon.setAttribute("class","icon_video span_icon");
+            icon.setAttribute("title", "Видео / GIF");
+            break;
+        case "post":
+            icon.setAttribute("class","icon_post span_icon");
+            icon.setAttribute("title", "Пост");
+            break;
+    }
+    div.appendChild(icon);
+    const strong = document.createElement("strong");
+    strong.innerText = card.title;
+    div.appendChild(strong);
+    if (card.type === "card" || card.type === "story") {
+        div.appendChild(document.createElement("br"));
+        const span = document.createElement("span");
+        span.innerText = card.description === undefined || card.description.length === 0 ? "Описание не указано" : card.description;
+        div.appendChild(span);
+    }
+    return div;
 }
 
 function hideAll() {
