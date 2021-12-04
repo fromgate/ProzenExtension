d1173d57de95e6294c5795256bf046246de96f27
fromgate <fromgate@gmail.com>
2021-12-04 22:43:05 +0300

    Update

--- prozen/js/content.js
+++ prozen/js/content.js
@@ -949,7 +949,7 @@ async function addInformerBlock() {
 
     if (bannedUsers) {
         const banCount = createElement("span", "Text Text_color_full Text_typography_text-14-18 author-studio-article-card__title prozen-mb5-block");
-        banCount.innerText = `Заблокировано: ${bannedUsers.bannedUsers.length}`;
+        banCount.innerText = `Заблокировано читателей: ${bannedUsers.bannedUsers.length}`;
         banCount.setAttribute("title", "Количество заблокированных комментаторов");
         informerContent.appendChild(banCount);
     }
