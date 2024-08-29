d1e5d6e34715e97105f5a0c0eca65b800db00e73
fromgate <fromgate@gmail.com>
2024-08-29 18:29:36 +0300

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,20 +18,6 @@ allprojects {
     }
 }
 
-
-/*
-
-
-fun extractVersionFromChangelog(file: File): String {
-
-
-    throw GradleException("Version not found in CHANGELOG.md")
-}
-
-val version = extractVersionFromChangelog(changelogFile)
- */
-
 tasks.named("build") {
-    //dependsOn(":prozen:packageExtension")
     dependsOn(":prozen-firefox:packageExtension")
 }
