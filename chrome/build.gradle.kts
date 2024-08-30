efea5fec5f6251754c0c834583185dc2e1219fc0
fromgate <fromgate@gmail.com>
2024-08-30 17:29:28 +0300

    Update

---
+++ chrome/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    js(IR) {
+        binaries.executable()
+        browser {}
+    }
+}
+
+
+/*
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+        plugins {
+            kotlin("multiplatform")
+        }
+
+kotlin {
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                outputFileName = "popupKt.js"
+                cssSupport {
+                    enabled = true
+                }
+                distribution {
+                    outputDirectory = File("$rootDir/build/distributions/jsKt")
+                }
+            }
+        }
+        binaries.executable()
+    }
+
+    sourceSets {
+        val jsMain by getting {
+            kotlin.srcDir("src/jsMain/kotlin")
+            dependencies {
+                implementation(project(":chrome"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.9.0-RC.2")
+            }
+        }
+    }
+}*/
