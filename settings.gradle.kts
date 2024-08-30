f2720219f565cbe2b0c983e1dd96b82e2bf6cbfb
fromgate <fromgate@gmail.com>
2024-08-30 22:28:41 +0300

    Update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "ProzenExtension"
 include(":page")
 include(":popup")
 include(":service-worker")
-//include(":types-chrome")
 include(":chrome")
 include(":prozen")
 include(":prozen-firefox")
