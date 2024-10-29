ec78c8681c88f285b2613a1b12176930b439d808
fromgate <fromgate@gmail.com>
2024-10-29 21:07:18 +0300

    Update

--- popup/build.gradle.kts
+++ popup/build.gradle.kts
@@ -25,6 +25,7 @@ kotlin {
                 implementation(project(":chrome"))
                 implementation(project(":common"))
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.9.0-RC.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.11.0")
             }
         }
     }
