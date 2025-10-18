465cd3b1f526fa5134279763bfbecf8980ae02ba
fromgate <fromgate@gmail.com>
2025-10-18 23:06:46 +0300

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "2.1.0" // apply false
-    kotlin("plugin.serialization") version "2.1.0" apply false
+    kotlin("multiplatform") version "2.2.20" // apply false
+    kotlin("plugin.serialization") version "2.2.20" apply false
 }
 
 ext["generateSourceMaps"] = true
